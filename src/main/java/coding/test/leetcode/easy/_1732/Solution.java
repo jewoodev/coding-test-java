4cb32f363ce131f870ef48d2dc86c6588d7b7d20
jewoodev <jewoos15@naver.com>
2024-12-14 21:06:06 +0900

    📰 leetcode: 1732. 최댓값 문제

---
+++ src/main/java/coding/test/leetcode/easy/_1732/Solution.java
@@ -0,0 +1,16 @@
+package coding.test.leetcode.easy._1732;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max = 0;
+        int sum = 0;
+        for (int i = 0; i < gain.length; i++) {
+            sum += gain[i];
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
