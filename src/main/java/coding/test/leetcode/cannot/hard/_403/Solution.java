b3c65d39b3c157aff137c3d23ca4c47bb04faa97
jewoodev <jewoos15@naver.com>
2024-12-15 16:24:44 +0900

    📰 leetcode: 403. 메모이제이션 + DP 풀이법 추가

--- src/main/java/coding/test/leetcode/cannot/hard/_403/Solution.java
+++ src/main/java/coding/test/leetcode/cannot/hard/_403/Solution.java
@@ -1,16 +1,34 @@
 package coding.test.leetcode.cannot.hard._403;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 class Solution {
     public boolean canCross(int[] stones) {
-        boolean flag = false;
-        int k = 0;
-        for (int i = 0; i < stones.length - 1; i++) {
-            if(stones[i+1] - stones[i] == k - 1 ||
-                    stones[i+1] - stones[i] == k ||
-                    stones[i+1] - stones[i] == k + 1) {
-                flag = true;
-            } else flag = false;
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+
+        // 각 돌 위치를 map에 저장하고 초기 상태를 설정
+        for (int stone : stones) {
+            map.put(stone, new HashSet<>());
         }
-        return flag;
+        map.get(0).add(1); // 첫 점프는 1로 시작
+
+        for (int stone : stones) {
+            for (int jump : map.get(stone)) {
+                int nextStone = stone + jump;
+                if (nextStone == stones[stones.length - 1]) {
+                    return true; // 마지막 돌에 도달하면 true 반환
+                }
+                if (map.containsKey(nextStone)) {
+                    if (jump - 1 > 0) map.get(nextStone).add(jump - 1);
+                    map.get(nextStone).add(jump);
+                    map.get(nextStone).add(jump + 1);
+                }
+            }
+        }
+
+        return false;
     }
 }
