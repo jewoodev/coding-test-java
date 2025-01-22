d0169ca183a054e6a0716af31911defc30f8086c
jewoodev <jewoos15@naver.com>
2025-01-22 15:31:36 +0900

    📰 JAVA로 배우는 자료구조: 7장. 이진 탐색 트리 구현체

---
+++ src/main/java/coding/test/datastructure_with/ch7/BinarySearchTree.java
@@ -0,0 +1,88 @@
+package coding.test.datastructure_with.ch7;
+
+public class BinarySearchTree {
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+
+    public static TreeNode insert(TreeNode root, int data) {
+        if (root == null) return new TreeNode(data);
+
+        if (data == root.data) return root;
+
+        if (data < root.data) root.left = insert(root.left, data);
+        else
+            root.right = insert(root.right, data);
+
+        return root;
+    }
+
+    public static TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return root;
+
+        if (key < root.data) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.data) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null && root.right == null)
+                root = null;
+            else if (root.left == null)
+                root = root.right;
+            else if (root.right == null)
+                root = root.left;
+            else {
+                TreeNode temp = findMin(root.right);
+                root.data = temp.data;
+                root.right = deleteNode(root.right, temp.data);
+            }
+        }
+        return root;
+    }
+
+    public static TreeNode findMin(TreeNode node) {
+        while (node.left != null)
+            node = node.left;
+        return node;
+    }
+
+    public static void inorder(TreeNode root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.printf("%d ", root.data);
+            inorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = null;
+
+        root = insert(root, 20);
+        root = insert(root, 15);
+        root = insert(root, 12);
+        root = insert(root, 25);
+        root = insert(root, 22);
+        root = insert(root, 21);
+        root = insert(root, 30);
+        System.out.printf("Inorder Traversal (Original Tree): ");
+        inorder(root);
+        System.out.printf("\n");
+        int keyToDelete = 20;
+        root = deleteNode(root, keyToDelete);
+        System.out.printf("Inorder Traversal (After Deleting %d): ", keyToDelete);
+        inorder(root);
+        System.out.printf("\n");
+        int keyToInsert = 30;
+        root = insert(root, keyToInsert);
+        System.out.printf("Inorder Traversal (After Inserting %d): ", keyToInsert);
+        inorder(root);
+        System.out.printf("\n");
+    }
+}
