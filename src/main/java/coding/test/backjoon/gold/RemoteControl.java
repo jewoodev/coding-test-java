b2c7b4687fc8db19418f291c9e9cc9366661b1ca
jewoodev <jewoos15@naver.com>
2025-08-26 16:42:38 +0900

    📝 update: 백준 1107 리모컨 문제 잘못된 풀이 수정

--- src/main/java/coding/test/backjoon/gold/RemoteControl.java
+++ src/main/java/coding/test/backjoon/gold/RemoteControl.java
@@ -4,6 +4,7 @@ import java.io.*;
 
 public class RemoteControl { // https://www.acmicpc.net/problem/1107
     static boolean[] broken = new boolean[10];
+
     static int possible(int c) {
         if (c == 0) {
             if (broken[0]) return 0;
@@ -13,10 +14,10 @@ public class RemoteControl { // https://www.acmicpc.net/problem/1107
         int len = 0;
         while (c > 0) {
             if (broken[c % 10]) return 0;
-
             len += 1;
             c /= 10;
         }
+
         return len;
     }
 
@@ -26,22 +27,31 @@ public class RemoteControl { // https://www.acmicpc.net/problem/1107
 
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        String[] temp = br.readLine().split(" ");
 
+        // M=0인 경우의 수정된 처리
+        if (M == 0) {
+            int directPress = String.valueOf(N).length();  // 숫자 버튼으로 직접 이동
+            int plusMinusOnly = Math.abs(N - 100);        // +/- 버튼으로만 이동
+            bw.write(Math.min(directPress, plusMinusOnly) + "\n");
+            bw.flush();
+            return;
+        }
+
+        String[] temp = br.readLine().split(" ");
         for (int i = 0; i < M; i++) {
             broken[Integer.parseInt(temp[i])] = true;
         }
 
-        int ans = N - 100;
-        if (ans < 0) ans = -ans;
+        // +/- 버튼으로만 이동하는 경우
+        int ans = Math.abs(N - 100);
 
+        // 숫자 버튼 조합 + +/- 버튼으로 이동하는 경우
+        // 탐색 범위를 더 넓혀서 확인
         for (int i = 0; i <= 1000000; i++) {
-            int c = i;
-            int len = possible(c);
+            int len = possible(i);
             if (len > 0) {
-                int press = c - N;
-                if (press < 0) press = -press;
-                if (ans > len + press) ans = len + press;
+                int press = Math.abs(i - N);
+                ans = Math.min(ans, len + press);
             }
         }
 
