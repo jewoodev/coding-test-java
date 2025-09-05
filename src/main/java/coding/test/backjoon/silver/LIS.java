18a2908d337e70998deed90f0bb69a845efb041c
jewoodev <jewoos15@naver.com>
2025-09-05 16:04:29 +0900

    📝 update: 백준 11053. 가장 긴 증가하는 부분 수열, DP -> 리팩토링

--- src/main/java/coding/test/backjoon/silver/LIS.java
+++ src/main/java/coding/test/backjoon/silver/LIS.java
@@ -7,23 +7,20 @@ public class LIS { // https://www.acmicpc.net/problem/11053, DP
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] a = new int[N + 1];
-        int[] d = new int[N + 1];
-        String[] read = br.readLine().split(" ");
-        for (int i = 1; i <= N; i++) {
-            a[i] = Integer.parseInt(read[i - 1]);
-        }
+        int[] a = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        int[] d = new int[N];
 
-        d[1] = 1;
-        for (int i = 2; i <= N; i++) {
-            for (int j = i; j >= 0; j--) {
-                if (a[i] > a[i - j] && d[i] < d[i - j] + 1)
-                    d[i] = d[i - j] + 1;
+        for (int i = 0; i < N; i++) {
+            d[i] = 1;
+            for (int j = i-1; j >= 0; j--) {
+                if (a[i] > a[j] && d[i] < d[j] + 1)
+                    d[i] = d[j] + 1;
             }
         }
 
         int ans = 0;
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             if (ans < d[i]) ans = d[i];
         }
         System.out.println(ans);
