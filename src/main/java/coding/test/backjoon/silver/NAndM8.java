be163a960d151da5eec8a5af44929ebd5f4dc0a0
jewoodev <jewoos15@naver.com>
2025-09-10 16:58:46 +0900

    📰 post: 백준 15657. N과 M (8), 브루트 포스 & 백트래킹

---
+++ src/main/java/coding/test/backjoon/silver/NAndM8.java
@@ -0,0 +1,43 @@
+package coding.test.backjoon.silver;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class NAndM8 { // https://www.acmicpc.net/problem/15657, 브루트 포스 & 백트래킹
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] a, c;
+
+    private static void go(int selected, int idx, int N, int M) throws IOException {
+        if (selected == M) {
+            for (int i: c) {
+                bw.write(i + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        if (idx > N) return;
+
+        for (int i = idx; i < N; i++) {
+            c[selected] = a[i];
+            go(selected + 1, i, N, M);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+
+        int[] read = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        int N = read[0];
+        int M = read[1];
+        a = new int[N];
+        c = new int[M];
+        a = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt).sorted().toArray();
+
+        go(0, 0, N, M);
+
+        bw.flush();
+    }
+}
