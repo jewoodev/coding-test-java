2b7105de00e20812f85466978e5abbc74d8d7dc8
jewoodev <jewoos15@naver.com>
2025-09-12 22:49:02 +0900

    📰 post: 백준 9095. 1, 2, 3 더하기 , 브루트 포스 || DP

---
+++ src/main/java/coding/test/backjoon/silver/Plus123.java
@@ -0,0 +1,25 @@
+package coding.test.backjoon.silver;
+
+import java.io.*;
+
+public class Plus123 { // https://www.acmicpc.net/problem/9095, 브루트 포스 || DP
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] d = new int[11];
+        d[0] = 1;
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (i - j >= 0) d[i] += d[i - j];
+            }
+        }
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            System.out.println(d[n]);
+        }
+    }
+}
+
