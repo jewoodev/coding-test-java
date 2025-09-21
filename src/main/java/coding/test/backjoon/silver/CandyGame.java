c48df49d854d8a496fddab9210a0547ba944a445
jewoodev <jewoos15@naver.com>
2025-09-21 11:55:40 +0900

    📝 update: 백준 16935. 사탕 게임, 가독성 ↑

--- src/main/java/coding/test/backjoon/silver/CandyGame.java
+++ src/main/java/coding/test/backjoon/silver/CandyGame.java
@@ -2,8 +2,28 @@ package coding.test.backjoon.silver;
 
 import java.io.*;
 
-public class CandyGame { // https://www.acmicpc.net/problem/3085
-    static int check(char[][] a, int startRow, int endRow, int startCol, int endCol) {
+public class CandyGame { // https://www.acmicpc.net/problem/3085, 브루트 포스
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        char[][] arr = new char[N][N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine().toCharArray();
+        }
+
+        int ans = 0;
+
+        ans = check(arr, 0, N - 1, 0, N - 1); // 원본 상태일 때 최대 사탕 수로 갱신
+
+        ans = findMaxCandyCnt(N, arr, ans); // 두 사탕 위치를 바꾸는 모든 경우의 수를 수행하여 최대 사탕 수 갱신
+
+        // (O($50^{3}$ = 약 12만) 의 시간 복잡도)
+
+        System.out.println(ans);
+    }
+
+    private static int check(char[][] a, int startRow, int endRow, int startCol, int endCol) {
         int n = a.length;
         int ans = 1;
 
@@ -38,42 +58,29 @@ public class CandyGame { // https://www.acmicpc.net/problem/3085
         return ans;
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-        char[][] arr = new char[N][N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = br.readLine().toCharArray();
-        }
-
-        int ans = 0;
-
-        ans= check(arr, 0, N - 1, 0, N - 1);
-
+    private static int findMaxCandyCnt(int N, char[][] arr, int ans) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (j + 1 < N && arr[i][j] != arr[i][j + 1]) {
+                if (j + 1 < N && arr[i][j] != arr[i][j + 1]) { // 하나의 열 씩 탐색하며 인접한 두 사탕이 다를 때마다 바꾸며
                     char t = arr[i][j];
                     arr[i][j] = arr[i][j + 1];
                     arr[i][j + 1] = t;
 
-                    int temp = check(arr, i, i, j, j + 1);
-                    if (ans < temp) ans = temp;
+                    int temp = check(arr, i, i, j, j + 1); // 먹을 수 있는 최대 사탕 수를
+                    if (ans < temp) ans = temp; // 갱신
 
-                    t = arr[i][j];
+                    t = arr[i][j]; // 작업이 끝나면, 원상 복구
                     arr[i][j] = arr[i][j + 1];
                     arr[i][j + 1] = t;
                 }
 
-                if (i + 1 < N && arr[i][j] != arr[i + 1][j]) {
+                if (i + 1 < N && arr[i][j] != arr[i + 1][j]) {  // 하나의 행 씩 탐색하며 동일하게
                     char t = arr[i][j];
                     arr[i][j] = arr[i + 1][j];
                     arr[i + 1][j] = t;
 
                     int temp = check(arr, i, i + 1, j, j);
-                    if (ans < temp) ans = temp;
+                    if (ans < temp) ans = temp; // 갱신
 
                     t = arr[i][j];
                     arr[i][j] = arr[i + 1][j];
@@ -81,8 +88,6 @@ public class CandyGame { // https://www.acmicpc.net/problem/3085
                 }
             }
         }
-
-        bw.write(ans + "\n");
-        bw.flush();
+        return ans;
     }
 }
