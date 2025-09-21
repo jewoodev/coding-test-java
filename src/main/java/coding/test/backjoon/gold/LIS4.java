48b4283dfbac5c8b8ef7a2217f23515b930cc269
jewoodev <jewoos15@naver.com>
2025-09-21 11:57:41 +0900

    📝 update: 백준 14002. 가장 긴 증가하는 부분 수열 4, DP & 역추적 -> 가독성 ↑

--- src/main/java/coding/test/backjoon/gold/LIS4.java
+++ src/main/java/coding/test/backjoon/gold/LIS4.java
@@ -1,43 +1,53 @@
 package coding.test.backjoon.gold;
 
 import java.io.*;
+import java.util.*;
 
-public class LIS4 { // https://www.acmicpc.net/problem/14002, DP
+public class LIS4 { // https://www.acmicpc.net/problem/14002, DP & 역추적
+    private static StringBuilder sb = new StringBuilder();
+    private static int[] v;
+    private static int[] a;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int[] a = new int[N];
+        a = new int[N];
+        v = new int[N];
         String[] read = br.readLine().split(" ");
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N; i++) {
             a[i] = Integer.parseInt(read[i]);
+            v[i] = -1;
+        }
 
         int[] d = new int[N];
-        d[0] = 1;
-        for (int i = 1; i < N; i++) {
-            for (int j = i; j > 0; j--) {
-                if (a[i] > a[i - j] && d[i] < d[i - j] + 1)
-                    d[i] = d[i - j] + 1;
+        for (int i = 0; i < N; i++) {
+            d[i] = 1;
+            for (int j = 0; j <= i; j++) {
+                if (a[i] > a[j] && d[i] < d[j] + 1) {
+                    d[i] = d[j] + 1;
+                    v[i] = j;
+                }
             }
         }
 
-        int i = 0;
         int ans = 0;
-        for (; i < N; i++) {
-            if (d[i] > ans) ans = d[i];
-        }
-
-        bw.write(ans + "\n");
-
-        int before = 0;
-        for (int j = 0; j < i; j++) {
-            if (a[j] > before) {
-                before = a[j];
-                bw.write(before + " ");
+        int p = 0;
+        for (int i = 0; i < N; i++) {
+            if (d[i] > ans) {
+                ans = d[i];
+                p = i;
             }
         }
 
-        bw.flush();
+        sb.append(ans).append("\n");
+        go(p);
+        System.out.print(sb);
+    }
+
+    private static void go(int p) throws IOException {
+        if (p == -1) return;
+        go(v[p]);
+        sb.append(a[p]).append(" ");
     }
 }
