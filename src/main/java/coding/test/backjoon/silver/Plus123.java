14732adcff20f1294d0e62efd1fa9d7cbd16b63a
jewoodev <jewoos15@naver.com>
2025-09-22 11:23:09 +0900

    📰 post: 백준 9095. 1, 2, 3 더하기 / 브루트 포스 풀이

--- src/main/java/coding/test/backjoon/silver/Plus123.java
+++ src/main/java/coding/test/backjoon/silver/Plus123.java
@@ -3,21 +3,31 @@ package coding.test.backjoon.silver;
 import java.io.*;
 
 public class Plus123 { // https://www.acmicpc.net/problem/9095, 브루트 포스 || DP
+    private static int ans = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] d = new int[11];
-        d[0] = 1;
-        for (int i = 1; i <= 10; i++)
-            for (int j = 1; j <= 3; j++)
-                if (i - j >= 0)
-                    d[i] += d[i - j];
-
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
 
-            System.out.println(d[n]);
+            ans = 0;
+            go(0, n, 0);
+            sb.append(ans).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static void go(int cur, int n, int len) {
+        if (len > 10) return;
+        if (cur == n) {
+            ans++;
+            return;
+        }
+        for (int i = 1; i <= 3; i++) {
+            go(cur + i, n, len + 1);
         }
     }
 }
