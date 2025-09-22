37da84a3b203da1780d7410dce40a58b849581d6
jewoodev <jewoos15@naver.com>
2025-09-22 16:02:39 +0900

    📰 post: 백준 15652. N과 M (4), 브루트 포스 & 백트래킹 -> 2회차 풀이

--- src/main/java/coding/test/backjoon/silver/NAndM4.java
+++ src/main/java/coding/test/backjoon/silver/NAndM4.java
@@ -1,43 +1,38 @@
 package coding.test.backjoon.silver;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
-public class NAndM4 { // https://www.acmicpc.net/problem/15652, 브루트 포스
-    static int[] cnt = new int[9];
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    static void go(int selected, int idx, int N, int M) throws IOException {
-        if (selected == M) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= cnt[i]; j++) {
-                    bw.write(i + " ");
-                }
-            }
-            bw.write("\n");
-            return;
-        }
-
-        if (idx > N) return;
-
-        for (int i = M - selected; i >= 1; i--) {
-            cnt[idx] = i;
-            go(selected + i, idx + 1, N, M);
-        }
-        cnt[idx] = 0;
-        go(selected, idx + 1, N, M);
-    }
+public class NAndM4 { // https://www.acmicpc.net/problem/15652, 브루트 포스 & 백트래킹
+    private static int[] seq;
+    private static int max, len;
+    private static StringBuilder ans = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] read = Arrays.stream(br.readLine().split(" "))
-                            .mapToInt(Integer::parseInt).toArray();
-        int N = read[0];
-        int M = read[1];
+        max = Integer.parseInt(st.nextToken());
+        len = Integer.parseInt(st.nextToken());
+        seq = new int[len];
 
-        go(0, 1, N, M);
+        go(0, 1);
 
-        bw.flush();
+        System.out.print(ans);
+    }
+
+    private static void go(int curLen, int s) {
+        if (curLen == len) {
+            for (int i : seq) {
+                ans.append(i).append(" ");
+            }
+            ans.append("\n");
+            return;
+        }
+
+        for (int i = s; i <= max; i++) {
+            seq[curLen] = i;
+            go(curLen + 1, i);
+        }
     }
 }
