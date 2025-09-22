17cf808547b20f06136efac62dc19c0b8c4937c8
jewoodev <jewoos15@naver.com>
2025-09-22 14:22:26 +0900

    📰 post: 백준 15650. N과 M (2), 브루트 포스 & 백트래킹 -> 2회차 풀이

--- src/main/java/coding/test/backjoon/silver/NAndM2.java
+++ src/main/java/coding/test/backjoon/silver/NAndM2.java
@@ -1,38 +1,38 @@
 package coding.test.backjoon.silver;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class NAndM2 { // https://www.acmicpc.net/problem/15650, 브루트 포스
-    static int[] a = new int[8];
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    static void go(int selected, int idx, int N, int M) throws IOException {
-        if (selected == M) {
-            for (int i = 0; i < 8; i++) {
-                if(a[i] != 0) bw.write(a[i] + " ");
-            }
-            bw.write("\n");
-            return;
-        }
-
-        if (idx > N) return;
-        a[selected] = idx;
-        go(selected + 1,idx + 1, N, M);
-        a[selected] = 0;
-        go(selected, idx + 1, N, M);
-    }
+    private static int[] seq;
+    private static int max, len;
+    private static StringBuilder ans = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        max = Integer.parseInt(st.nextToken());
+        len = Integer.parseInt(st.nextToken());
+        seq = new int[len];
 
-        int[] read = Arrays.stream(br.readLine().split(" "))
-                            .mapToInt(Integer::parseInt).toArray();
-        int N = read[0];
-        int M = read[1];
+        go(1,0);
 
-        go(0, 1, N, M);
+        System.out.print(ans);
+    }
 
-        bw.flush();
+    private static void go(int s, int curLen) {
+        if (curLen == len) {
+            for (int i : seq) {
+                ans.append(i).append(" ");
+            }
+            ans.append("\n");
+            return;
+        }
+
+        for (int i = s; i <= max; i++) {
+            seq[curLen] = i;
+            go(i + 1, curLen + 1);
+        }
     }
 }
