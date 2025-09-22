e7d526a3ae977563f96fde9e1bde2c0a22dd711a
jewoodev <jewoos15@naver.com>
2025-09-22 16:03:11 +0900

    📰 post: 백준 15655. N과 M (6), 브루트 포스 & 백트래킹 -> 2회차 풀이

--- src/main/java/coding/test/backjoon/silver/NAndM6.java
+++ src/main/java/coding/test/backjoon/silver/NAndM6.java
@@ -1,50 +1,44 @@
 package coding.test.backjoon.silver;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class NAndM6 { // https://www.acmicpc.net/problem/15655, 브루트 포스 & 백트래킹
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int[] a, c;
-    static boolean[] b;
-
-    static void go(int selected, int idx, int N, int M) throws IOException {
-        if (selected == M) {
-            for (int i: c) {
-                bw.write(i + " ");
-            }
-            bw.write("\n");
-            return;
-        }
-
-        if (idx > N) return;
-
-        for (int i = idx; i < N; i++) {
-            if (b[i]) continue;
-
-            c[selected] = a[i];
-            b[i] = true;
-            go(selected + 1, i + 1, N, M);
-            b[i] = false;
-        }
-    }
+    private static int[] seq, sub;
+    private static int cnt, len;
+    private static StringBuilder ans = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] read = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt).toArray();
-        int N = read[0];
-        int M = read[1];
-        a = new int[N];
-        c = new int[M];
-        b = new boolean[N];
+        cnt = Integer.parseInt(st.nextToken());
+        len = Integer.parseInt(st.nextToken());
+        sub = new int[len];
+        seq = new int[cnt];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < cnt; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(seq);
 
-        a = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt).sorted().toArray();
+        go(0, 0);
 
-        go(0, 0, N, M);
+        System.out.print(ans);
+    }
 
-        bw.flush();
+    private static void go(int curLen, int s) {
+        if (curLen == len) {
+            for (int i : sub) {
+                ans.append(i).append(" ");
+            }
+            ans.append("\n");
+            return;
+        }
+
+        for (int i = s; i < cnt; i++) {
+            sub[curLen] = seq[i];
+            go(curLen + 1, i + 1);
+        }
     }
 }
