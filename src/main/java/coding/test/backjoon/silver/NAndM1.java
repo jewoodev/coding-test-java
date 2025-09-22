89e4c34c0b49f93ca1b84bea1349beee364615a0
jewoodev <jewoos15@naver.com>
2025-09-22 14:21:13 +0900

    📰 post: 백준 15649. N과 M (1), 브루트 포스 & 백트래킹 -> 2회차 풀이

--- src/main/java/coding/test/backjoon/silver/NAndM1.java
+++ src/main/java/coding/test/backjoon/silver/NAndM1.java
@@ -4,40 +4,38 @@ import java.io.*;
 import java.util.*;
 
 public class NAndM1 { // https://www.acmicpc.net/problem/15649, 브루트 포스
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int[] a;
-    static boolean[] b;
-
-    static void go(int idx, int N, int M) throws IOException {
-        if (idx == M) {
-            for (int i: a) {
-                bw.write(i + " ");
-            }
-            bw.write("\n");
-            return;
-        }
-
-        for (int i = 1; i <= N; i++) {
-            if (b[i]) continue;
-            a[idx] = i;
-            b[i] = true;
-            go(idx + 1, N, M);
-            b[i] = false;
-        }
-    }
+    private static StringBuilder ans = new StringBuilder();
+    private static int[] seq;
+    private static boolean[] selected;
+    private static int n, m;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] read = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt).toArray();
-        int N = read[0];
-        int M = read[1];
-        a = new int[M];
-        b = new boolean[N + 1];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        seq = new int[m];
+        selected = new boolean[10];
+        go(0);
+        System.out.print(ans);
+    }
 
-        go(0, N, M);
+    private static void go(int len) {
+        if (len == m) {
+            for (int i : seq) {
+                ans.append(i).append(" ");
+            }
+            ans.append("\n");
+            return;
+        }
 
-        bw.flush();
+        for (int i = 1; i <= n; i++) {
+            if (selected[i]) continue;
+            selected[i] = true;
+            seq[len] = i;
+            go(len + 1);
+            selected[i] = false;
+        }
     }
 }
