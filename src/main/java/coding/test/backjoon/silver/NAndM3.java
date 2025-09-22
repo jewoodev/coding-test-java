1a337b2f1c37b066b2038f2daf7f1db57689ed18
jewoodev <jewoos15@naver.com>
2025-09-22 16:02:13 +0900

    📰 post: 백준 15651. N과 M (3), 브루트 포스 & 백트래킹 -> 2회차 풀이

--- src/main/java/coding/test/backjoon/silver/NAndM3.java
+++ src/main/java/coding/test/backjoon/silver/NAndM3.java
@@ -3,35 +3,36 @@ package coding.test.backjoon.silver;
 import java.io.*;
 import java.util.*;
 
-public class NAndM3 { // https://www.acmicpc.net/problem/15651, 브루트 포스
-    static int[] a = new int[7];
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    static void go(int idx, int N, int M) throws IOException {
-        if (idx == M) {
-            for (int i = 0; i < idx; i++) {
-                bw.write(a[i] + " ");
-            }
-            bw.write("\n");
-            return;
-        }
-
-        for (int i = 1; i <= N; i++) {
-            a[idx] = i;
-            go(idx + 1, N, M);
-        }
-    }
+public class NAndM3 { // https://www.acmicpc.net/problem/15651, 브루트 포스 & 백트래킹
+    private static StringBuilder ans = new StringBuilder();
+    private static int max, len;
+    private static int[] seq;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] read = Arrays.stream(br.readLine().split(" "))
-                            .mapToInt(Integer::parseInt).toArray();
-        int N = read[0];
-        int M = read[1];
+        max = Integer.parseInt(st.nextToken());
+        len = Integer.parseInt(st.nextToken());
+        seq = new int[len];
 
-        go(0, N, M);
+        go(0);
 
-        bw.flush();
+        System.out.print(ans);
+    }
+
+    private static void go(int curLen) {
+        if (curLen == len) {
+            for (int i : seq) {
+                ans.append(i).append(" ");
+            }
+            ans.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= max; i++) {
+            seq[curLen] = i;
+            go(curLen + 1);
+        }
     }
 }
