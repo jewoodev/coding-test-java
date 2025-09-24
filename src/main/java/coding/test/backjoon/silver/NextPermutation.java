c050727849d4ff5a9d8e940aedba75f628ef7747
jewoodev <jewoos15@naver.com>
2025-09-24 14:54:05 +0900

    📝 update: 백준 10972. 다음 순열, 수학 & 조합론 -> 3회차 풀이

--- src/main/java/coding/test/backjoon/silver/NextPermutation.java
+++ src/main/java/coding/test/backjoon/silver/NextPermutation.java
@@ -8,8 +8,8 @@ public class NextPermutation { // https://www.acmicpc.net/problem/10972, 수학 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
@@ -25,22 +25,17 @@ public class NextPermutation { // https://www.acmicpc.net/problem/10972, 수학 
         System.out.println(sb);
     }
 
-    public static boolean nextPermutation(int[] arr) {
-        int n = arr.length;
-
+    private static boolean nextPermutation(int[] arr) {
         // 1단계: 뒤에서부터 arr[i] < arr[i+1]인 가장 큰 i 찾기
-        int i = n - 2;
+        int i = arr.length - 2;
         while (i >= 0 && arr[i] >= arr[i + 1]) {
             i--;
         }
-
         // 마지막 순열인 경우 (내림차순)
-        if (i < 0) {
-            return false;
-        }
+        if (i < 0) return false;
 
         // 2단계: arr[i]보다 큰 가장 작은 원소를 뒤에서부터 찾기
-        int j = n - 1;
+        int j = arr.length - 1;
         while (arr[j] <= arr[i]) {
             j--;
         }
@@ -49,15 +44,15 @@ public class NextPermutation { // https://www.acmicpc.net/problem/10972, 수학 
         swap(arr, i, j);
 
         // 4단계: i+1부터 끝까지 뒤집기 (오름차순 정렬)
-        reverse(arr, i + 1, n - 1);
+        reverse(arr, i + 1, arr.length - 1);
 
         return true;
     }
 
     private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
+        int tmp = arr[i];
         arr[i] = arr[j];
-        arr[j] = temp;
+        arr[j] = tmp;
     }
 
     private static void reverse(int[] arr, int start, int end) {
