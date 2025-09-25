fe2c9a4e16bfe86490dfc602ae4aa3430f8526de
jewoodev <jewoos15@naver.com>
2025-09-25 16:37:45 +0900

    📝 update: 백준 14889. 스타트와 링크 -> 비트마스크 풀이

--- src/main/java/coding/test/backjoon/silver/StartAndLink.java
+++ src/main/java/coding/test/backjoon/silver/StartAndLink.java
@@ -3,70 +3,61 @@ package coding.test.backjoon.silver;
 import java.io.*;
 import java.util.*;
 
-public class StartAndLink { // https://www.acmicpc.net/problem/14889, 브루트포스 & 백트래킹
-    private static int[][] S;
+public class StartAndLink { // https://www.acmicpc.net/problem/14889, 브루트포스 & 백트래킹 | 비트마스킹
+    private static int[][] s;
     private static int n;
     private static int minDiff = Integer.MAX_VALUE;
+    private static List<Integer> start, link;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        start = new ArrayList<>();
+        link = new ArrayList<>();
         n = Integer.parseInt(br.readLine());
-        S = new int[n][n];
+        s = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                S[i][j] = Integer.parseInt(st.nextToken());
+                s[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 조합을 사용하여 스타트 팀 선택
-        boolean[] selected = new boolean[n];
-        combination(0, 0, selected);
+        // 비트마스킹을 사용하여 스타트 팀 선택
+        bitmasking();
 
         System.out.println(minDiff);
     }
 
     // 조합: n개 중 n / 2개 선택
-    private static void combination(int idx, int count, boolean[] selected) {
-        if (count == n / 2) {
-            calculateDifference(selected);
-            return;
+    private static void bitmasking() {
+        for (int i = 0; i < (1 << n); i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    start.add(j);
+                }
+                else link.add(j);
+            }
+            if (start.size() == n / 2) {
+                calculateDiff();
+            }
+            start.clear();
+            link.clear();
         }
-
-        if (idx == n) return;
-
-        // 현재 사람을 스타트 팀에 포함
-        selected[idx] = true;
-        combination(idx + 1, count + 1, selected);
-
-        // 현재 사람을 스타트 팀에 포함하지 않음
-        selected[idx] = false;
-        combination(idx + 1, count, selected);
     }
 
-    private static void calculateDifference(boolean[] selected) {
+    private static void calculateDiff() {
         int startSum = 0;
         int linkSum = 0;
+        int half = n / 2;
 
-        for (int i = 0; i < n; i++) {
-            // 스타트 팀의 능력치 계산
-            if (selected[i]) {
-                for (int j = 0; j < n; j++) {
-                    if (selected[j]) {
-                        startSum += S[i][j];
-                    }
-                }
-            }
-
-            // 링크 팀의 능력치 계산
-            else {
-                for (int j = 0; j < n; j++) {
-                    if (!selected[j]) {
-                        linkSum += S[i][j];
-                    }
-                }
+        for (int i = 0; i < half; i++) {
+            for (int j = i + 1; j < half; j++) {
+                startSum += s[start.get(i)][start.get(j)];
+                startSum += s[start.get(j)][start.get(i)];
+                linkSum += s[link.get(i)][link.get(j)];
+                linkSum += s[link.get(j)][link.get(i)];
             }
         }
 
