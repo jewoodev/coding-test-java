a572bf4ac258764603077cbb971226ec01dba2fd
jewoodev <jewoos15@naver.com>
2025-10-06 15:47:10 +0900

    📝 update: 백준 1260. DFS와 BFS, 그래프 & DFS & BFS

--- src/main/java/coding/test/backjoon/silver/DFSAndBFS.java
+++ src/main/java/coding/test/backjoon/silver/DFSAndBFS.java
@@ -4,17 +4,55 @@ import java.io.*;
 import java.util.*;
 
 public class DFSAndBFS { // https://www.acmicpc.net/problem/1260, 그래프 & BFS & DFS
-    private static StringBuilder sb = new StringBuilder();
-    private static List<Integer>[] g;
+    private static StringBuilder ans = new StringBuilder();
+    private static List<Integer>[] em; // 간선 행렬
     private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        em = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            em[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            em[x].add(y);
+            em[y].add(x);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(em[i]);
+        }
+
+        // dfs
+        visited = new boolean[n+1];
+        dfs(v);
+        ans.append("\n");
+
+        // bfs
+        visited = new boolean[n+1];
+        bfs(v);
+        ans.append("\n");
+        System.out.println(ans);
+    }
 
     private static void dfs(int cur) {
         if (visited[cur]) return;
 
         visited[cur] = true;
-        sb.append(cur).append(" ");
-        for (int i : g[cur])
+        ans.append(cur).append(" ");
+        for (int i : em[cur]) {
             dfs(i);
+        }
     }
 
     private static void bfs(int v) {
@@ -24,47 +62,14 @@ public class DFSAndBFS { // https://www.acmicpc.net/problem/1260, 그래프 & BF
         q.offer(v);
         while (!q.isEmpty()) {
             int c = q.poll();
-            sb.append(c).append(" ");
+            ans.append(c).append(" ");
 
-            for (int i : g[c]) {
+            for (int i : em[c]) {
                 if (!visited[i]) {
                     visited[i] = true;
                     q.offer(i);
                 }
             }
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] read = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt).toArray();
-        int n = read[0], m = read[1], v = read[2];
-        g = new ArrayList[n+1];
-        for (int i = 1; i <= n; i++)
-            g[i] = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            read = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(Integer::parseInt).toArray();
-            int x = read[0];
-            int y = read[1];
-            g[x].add(y);
-            g[y].add(x);
-        }
-
-        for (int i = 1; i <= n; i++)
-            Collections.sort(g[i]);
-
-        // dfs
-        visited = new boolean[n+1];
-        dfs(v);
-        sb.append("\n");
-
-        // bfs
-        visited = new boolean[n+1];
-        bfs(v);
-        sb.append("\n");
-        System.out.println(sb);
     }
 }
