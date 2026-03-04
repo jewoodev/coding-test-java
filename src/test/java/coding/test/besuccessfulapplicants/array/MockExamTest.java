06c9e3e1caeff221eb35a5add19e9efce43a4296
jewoodev <jewoos15@naver.com>
2026-03-04 21:03:03 +0900

    update: 합격자 되기, 모의고사 테스트 코드 수정

--- src/test/java/coding/test/besuccessfulapplicants/array/MockExamTest.java
+++ src/test/java/coding/test/besuccessfulapplicants/array/MockExamTest.java
@@ -2,17 +2,23 @@ package coding.test.besuccessfulapplicants.array;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MockExamTest {
     @Test
     void test() {
-        var sut = new MockExam();
-        int[][] args = {
-            {1,2,3,4,5},
-            {1,3,2,4,2}
+        int[][] answers = {
+                {1, 2, 3, 4, 5},
+                {1, 3, 2, 4, 2}
         };
+        int[][] expected = {
+                {1},
+                {1, 2, 3}
+        };
+        var sut = new MockExam();
 
-        for (int[] arg : args) {
-            sut.print(sut.solution(arg));
+        for (int i = 0; i < answers.length; i++) {
+            assertThat(sut.solution(answers[i])).isEqualTo(expected[i]);
         }
     }
 }
