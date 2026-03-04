8fa79ca600da16a159bd08099b62e22c90e4b0b3
jewoodev <jewoos15@naver.com>
2026-03-04 17:08:38 +0900

    update: 합격자 되기, 모의고사 (2회차)

--- src/main/java/coding/test/besuccessfulapplicants/array/MockExam.java
+++ src/main/java/coding/test/besuccessfulapplicants/array/MockExam.java
@@ -1,45 +1,32 @@
 package coding.test.besuccessfulapplicants.array;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 class MockExam { // https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java,
+    private static int[][] patterns = {
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
+    };
+    private static int[] length = {5,8,10};
+
     public int[] solution(int[] answers) {
-
-        int[][] pattern = {
-                {1, 2, 3, 4, 5},
-                {2, 1, 2, 3, 2, 4, 2, 5},
-                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-        };
-
         int[] scores = new int[3];
 
-
         for (int i = 0; i < answers.length; i++) {
-            for (int j = 0; j < pattern.length; j++) {
-                if (answers[i] == pattern[j][i % pattern[j].length]) {
+            for (int j = 0; j < 3; j++) {
+                if (patterns[j][i % length[j]] == answers[i]) {
                     scores[j]++;
                 }
             }
         }
-
         int maxScore = Arrays.stream(scores).max().getAsInt();
 
-        ArrayList<Integer> answer = new ArrayList<>();
+        List<Integer> scorer = new ArrayList<>();
         for (int i = 0; i < scores.length; i++) {
-            if (scores[i] == maxScore) {
-                answer.add(i + 1);
-            }
+            if (maxScore == scores[i])
+                scorer.add(i);
         }
-
-        return answer.stream().mapToInt(Integer::intValue).toArray();
-    }
-
-    public void print(int[] arr) {
-        StringBuilder sb = new StringBuilder();
-        for (int i : arr) {
-            sb.append(i).append(" ");
-        }
-        System.out.println(sb);
+        return scorer.stream().mapToInt(i -> i + 1).toArray();
     }
 }
