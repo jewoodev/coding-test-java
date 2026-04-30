171d19634ac67aadd3030f14520dc3c227a6247a
jewoodev <jewoos15@naver.com>
2026-04-30 16:03:57 +0900

    update: 기능개발 (더 나은 풀이)

--- src/main/java/coding/test/besuccessfulapplicants/queue/devfeat/DevelopFeature.java
+++ src/main/java/coding/test/besuccessfulapplicants/queue/devfeat/DevelopFeature.java
@@ -3,56 +3,23 @@ package coding.test.besuccessfulapplicants.queue.devfeat;
 import java.util.*;
 
 class DevelopFeature {
-    private static int order = 0;
-
     public int[] solution(int[] progresses, int[] speeds) {
         var answer = new ArrayList<Integer>();
-        var q = new ArrayDeque<Feature>();
+        var days = new ArrayDeque<Integer>();
         for (int i = 0; i < speeds.length; i++) {
-            q.offerLast(new Feature(progresses[i], i));
+            days.offer((int) Math.ceil(100.0 - progresses[i]) / speeds[i]);
         }
 
-        while (!q.isEmpty()) {
-            var f = q.pollFirst();
-            if (f.p > 99) {
-                if (f.idx == order) {
-                    q.offerLast(f);
-                    distribute(f, q, answer);
-                }
-                else q.offerLast(f);
+        while (!days.isEmpty()) {
+            int front = days.poll();
+            int cnt = 1;
+            while (!days.isEmpty() && days.peek() <= front) {
+                days.poll();
+                cnt++;
             }
-            else q.offerLast(f.updateFeat(speeds[f.idx]));
+            answer.add(cnt);
         }
 
         return answer.stream().mapToInt(Integer::intValue).toArray();
-    }
-
-    private void distribute(Feature f, Deque<Feature> q, List<Integer> answer) {
-        int cnt = 1;
-        while (!q.isEmpty()) {
-            var now = q.pollFirst();
-            order++;
-            if (now == f) break;
-            else if (now.p > 99 && now.idx == order) cnt++;
-            else {
-                order--;
-                q.offerLast(now);
-            }
-        }
-        answer.add(cnt);
-    }
-
-    static class Feature {
-        int p, idx;
-
-        private Feature updateFeat(int plus) {
-            this.p += plus;
-            return this;
-        }
-
-        private Feature(int p, int idx) {
-            this.p = p;
-            this.idx = idx;
-        }
     }
 }
